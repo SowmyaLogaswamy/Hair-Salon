d58a40fbf344f8cf24cc6df7d024193f5a2826a4
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-07-14 18:50:17 -0700

    Add functionality to add a new Stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,15 @@ public static void main(String[] args) {
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
+  // get("/stylist/add", (request,response) -> {
+  // Map<String, Object> model = new HashMap<String, Object>();
+  // Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylistId")));
+  // model.put("stylists", Stylist.all());
+  // model.put("clients", Client.all());
+  //     model.put("template", "templates/index.vtl");
+  //     return new ModelAndView(model, layout);
+  //   }, new VelocityTemplateEngine());
+
 get("/stylist/:id", (request, response) -> {
   Map<String, Object> model = new HashMap<String, Object>();
   Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
@@ -39,35 +48,39 @@ get("/stylist/:stylistId/client/:id", (request, response) -> {
 
 
 get("/stylists/new", (request, response) -> {
-  Map<String, Object> model = new HashMap<String, Object>();
-  model.put("template", "templates/new-stylist-form.vtl");
-  return new ModelAndView(model, layout);
-}, new VelocityTemplateEngine());
-
-post("/success", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      //create Artist object
-      String userArtist = request.queryParams("user-artist");
-      Artist artist = new Artist(userArtist);
-      artist.save();
-      //create Album object
-      String userStylistName = request.queryParams("user-album-name");
-      int userAlbumYear = Integer.parseInt(request.queryParams("user-album-year"));
-      String userAlbumGenre = request.queryParams("user-album-genre");
-      Album album = new Album(userAlbumName, userAlbumYear, userAlbumGenre, artist.getId());
-      album.save();
-      //create Review object
-      String stylistName = request.queryParams("name");
-      String stylistDescription = request.queryParams("comment");
-      int reviewRating = Integer.parseInt(request.queryParams("rating"));
-      Review newReview = new Review(reviewName, reviewComment, reviewRating, album.getId());
-      newReview.save();
-      model.put("artist", artist);
-      model.put("album", album);
-      model.put("reviews", Review.findAlbumId(album.getId()));
-      model.put("template", "templates/album.vtl");
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylist-new.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/stylists-form", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+ //    get("/stylists/add", (request, response) -> {
+ //   Map<String, Object> model = new HashMap<String, Object>();
+ //   model.put("stylists", Stylist.all());
+ //   model.put("template", "templates/tasks.vtl");
+ //   return new ModelAndView(model, layout);
+ // }, new VelocityTemplateEngine());
+ //
+ //    post("/stylists/add/", (request, response) -> {
+ //      Map<String, Object> model = new HashMap<String, Object>();
+ //      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
+ //      String name = request.queryParams("name");
+ //      String description = request.queryParams("description");
+ //      Stylist newStylist = new Stylist(name, description);
+ //      newStylist.save();
+ //      model.put("stylist", stylist);
+ //      model.put("template", "templates/index.vtl");
+ //      return new ModelAndView(model, layout);
+ //    }, new VelocityTemplateEngine());
+
+
 
 
 
