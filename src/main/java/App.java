768952306564e6b120dadaef754cf9a1bbbff568
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-07-14 21:17:52 -0700

    Add a new stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -61,29 +61,24 @@ get("/stylists/new", (request, response) -> {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
- //    get("/stylists/add", (request, response) -> {
- //   Map<String, Object> model = new HashMap<String, Object>();
- //   model.put("stylists", Stylist.all());
- //   model.put("template", "templates/tasks.vtl");
- //   return new ModelAndView(model, layout);
- // }, new VelocityTemplateEngine());
- //
- //    post("/stylists/add/", (request, response) -> {
- //      Map<String, Object> model = new HashMap<String, Object>();
- //      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":stylist_id")));
- //      String name = request.queryParams("name");
- //      String description = request.queryParams("description");
- //      Stylist newStylist = new Stylist(name, description);
- //      newStylist.save();
- //      model.put("stylist", stylist);
- //      model.put("template", "templates/index.vtl");
- //      return new ModelAndView(model, layout);
- //    }, new VelocityTemplateEngine());
+//  get("/stylists/add", (request, response) -> {
+//    Map<String, Object> model = new HashMap<String, Object>();
+//    model.put("stylists", Stylist.all());
+//    model.put("template", "templates/tasks.vtl");
+//    return new ModelAndView(model, layout);
+//  }, new VelocityTemplateEngine());
 
-
-
-
-
-
+     post("/stylists/add", (request, response) -> {
+       Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+     String description = request.queryParams("description");
+      Stylist newStylist = new Stylist(name, description);
+     newStylist.save();
+       //model.put("stylist", newStylist);
+       model.put("stylists", Stylist.all());
+       model.put("clients", Client.all());
+       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+     }, new VelocityTemplateEngine());
 }
 }
